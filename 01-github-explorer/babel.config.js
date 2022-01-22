4bfe2caef7dc62b72e9005e30e5701f2ddff4f30
ErildoJS <patriciopatrick92@gmail.com>
2022-01-22 23:16:50 +0100

    all configurations are done

--- 01-github-explorer/babel.config.js
+++ 01-github-explorer/babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    '@babel/preset-react'
+    ['@babel/preset-react', {
+      runtime: 'automatic'/** possibilita nao importar mais o react nod arqs */
+    }]
   ]
 }
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,21 +1,39 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "index.jsx"),/** arq de entrada */
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map': 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),/** arq de entrada */
   output: {
-    path: path.resolve(__dirname, "dist"),/** arq que vou gerar */
-    filename: "bundle.js",/** nome do arq gerado */
+    path: path.resolve(__dirname, 'dist'),/** arq que vou gerar */
+    filename: 'bundle.js',/** nome do arq gerado */
   },
   resolve: {
     extensions: ['.js', '.jsx'],/** arq que ele pode ler */
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    })
+  ],
   module: {/** como a nossa app vai se comportar quando estivermos a importar os arqs */
      rules: [
        {
          test: /\.jsx$/,
          exclude: /node_modules/,
          use: 'babel-loader'/**integracao entre o babel eo webpack */
-       }
+       },
+       {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']/**integracao entre o babel eo webpack e css */
+      }
      ]
   }
 }
